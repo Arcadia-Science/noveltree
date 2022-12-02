71653da78e4aeb7ee40515cadf81e9fe478cca78
Ubuntu <ubuntu@ip-172-31-22-227.us-west-1.compute.internal>
2022-12-02 19:59:02 +0000

    Created uniprot.ws docker image, made variable names consistent.

--- bin/UniProt-Protein-Annotation-NF.R
+++ bin/UniProt-Protein-Annotation-NF.R
@@ -1,13 +1,6 @@
 #!/usr/bin/env Rscript
-
-# Libraries to get annotations using the uniprot web service
-# The docker image on quay.io is out of date, so pull a docker image of
-# bioconductor, and then install
-# Be sure to set the library path to a temporary directory first. 
-dir.create('./Rlibs', showWarnings = FALSE)
-.libPaths('./Rlibs')
-BiocManager::install('UniProt.ws', lib = './Rlibs')
-library(UniProt.ws, lib = './Rlibs')
+# Library to get annotations using the uniprot web service
+library(UniProt.ws)
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -19,23 +12,23 @@ spp <- args[1]
 ids <- args[2]
 
 # Because this is going to be a fair bit of information, let's make a new directory to house these outputs/gene ontologies. 
-annotDir <- './annotations/'
-dir.create(file.path(annotDir), showWarnings = FALSE) # Will only create the directory if it doesn't already exist. 
+annot_dir <- './annotations/'
+dir.create(file.path(annot_dir), showWarnings = FALSE) # Will only create the directory if it doesn't already exist. 
 
 # Specify the fields that we would like to download. 
-seqMeta <- 
+seq_meta <- 
     c('organism_name', 'organism_id', 'accession', 
     'protein_name', 'length', 'mass')
 
 gos <- 
     c('go_p', 'go_c', 'go_f', 'go_id')
     
-famDomains <- 
+fam_domains <- 
     c('xref_disprot','xref_ideal','xref_interpro',
     'xref_panther','xref_pirsf','xref_pfam',
     'xref_sfld','xref_supfam','xref_tigrfams')
 
-ogDBs <- 
+og_dbs <- 
     c('xref_inparanoid','xref_orthodb','xref_phylomedb',
     'xref_treefam','xref_eggnog')
 
@@ -43,15 +36,15 @@ ogDBs <-
 accessions <- read.table(ids, sep = '\t')$V1
 
 # Get the species tax ID by pulling out into for the first accession
-taxID <- UniProt.ws::queryUniProt(paste0("accession:", accessions[1]), fields = 'organism_id')[[1]]
+tax_id <- UniProt.ws::queryUniProt(paste0("accession:", accessions[1]), fields = 'organism_id')[[1]]
 # Pull down annotations for proteins in this species 
-up <- UniProt.ws::UniProt.ws(taxID)
+up <- UniProt.ws::UniProt.ws(tax_id)
 
 # And pull down annotations, removing rows for proteins without any annotations. 
-annots <- UniProt.ws::select(up, accessions, c(seqMeta, gos, famDomains, ogDBs), 'UniProtKB')
-toDrop <- which(rowSums(is.na(annots[,-c(1:2)])) == ncol(annots[,-c(1:2)]))
-if(length(toDrop) > 0){
-    annots <- annots[-toDrop,]
+annots <- UniProt.ws::select(up, accessions, c(seq_meta, gos, fam_domains, og_dbs), 'UniProtKB')
+to_drop <- which(rowSums(is.na(annots[,-c(1:2)])) == ncol(annots[,-c(1:2)]))
+if(length(to_drop) > 0){
+    annots <- annots[-to_drop,]
 }
 colnames(annots) <- 
     gsub("[.][.]", "_", colnames(annots)) |> 
@@ -59,7 +52,8 @@ colnames(annots) <-
         sub(pattern = "_$", replacement = "")
 
 # Write out to a tsv.
-write.table(annots, file = paste0(annotDir, spp, '-protein-annotations.tsv'), col.names = T, row.names = F, sep = '\t', quote = F)
+write.table(annots, file = paste0(annot_dir, spp, '-protein-annotations.tsv'), 
+            col.names = T, row.names = F, sep = '\t', quote = F)
 
 sink("version.txt")
 packageVersion("UniProt.ws")
