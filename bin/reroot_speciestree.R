7b845c3fe696aee32eab20de73f50c1abd3abb1a
Ubuntu <austin.patton@arcadiascience.com>
2023-05-27 00:11:16 +0000

    simple R script to root asteroid tree using user-specified outgroups

---
+++ bin/reroot_speciestree.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly = TRUE)
+
+# Extract arguments
+asteroid_tree <- args[1]
+outgroups <- args[2]
+outgroups <- unlist(strsplit(outgroups, ","))
+
+# Load necessary libraries
+library(ape)
+library(phytools)
+
+# Read in the newick tree file
+tree <- unroot(read.tree(asteroid_tree))
+
+# Reroot the tree
+if(length(outgroups) > 1){
+    tree$node.label <- 1:tree$Nnode
+    mrca <- getMRCA(tree, outgroups)
+    tree <- reroot(tree, node = mrca, position = 0.5)
+} else {
+    tree$node.label <- 1:tree$Nnode
+    parent_node <- 
+        tree$edge[,1][which(tree$edge[,2] == match(outgroups, tree$tip.label))]
+    tree <- reroot(tree, node = parent_node, position = 0.5)
+}
+
+# Write the rerooted tree to a file
+write.tree(tree, file = "asteroid_rooted.bestTree.newick")
