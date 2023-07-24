b75c79e9a28daaa764530ab4438db98d1ffe73ee
Ubuntu <austin.patton@arcadiascience.com>
2023-07-24 18:45:36 +0000

    specify source package when calling specialty functions

--- bin/reroot_speciestree.R
+++ bin/reroot_speciestree.R
@@ -11,19 +11,19 @@ library(ape)
 library(phytools)
 
 # Read in the newick tree file
-tree <- unroot(read.tree(asteroid_tree))
+tree <- phytools::unroot(ape::read.tree(asteroid_tree))
 
 # Reroot the tree
 if(length(outgroups) > 1){
     tree$node.label <- 1:tree$Nnode
-    mrca <- getMRCA(tree, outgroups)
-    tree <- reroot(tree, node = mrca, position = 0.5)
+    mrca <- ape::getMRCA(tree, outgroups)
+    tree <- phytools::reroot(tree, node = mrca, position = 0.5)
 } else {
     tree$node.label <- 1:tree$Nnode
     parent_node <- 
         tree$edge[,1][which(tree$edge[,2] == match(outgroups, tree$tip.label))]
-    tree <- reroot(tree, node = parent_node, position = 0.5)
+    tree <- phytools::reroot(tree, node = parent_node, position = 0.5)
 }
 
 # Write the rerooted tree to a file
-write.tree(tree, file = "asteroid_rooted.bestTree.newick")
+ape::write.tree(tree, file = "asteroid_rooted.bestTree.newick")
